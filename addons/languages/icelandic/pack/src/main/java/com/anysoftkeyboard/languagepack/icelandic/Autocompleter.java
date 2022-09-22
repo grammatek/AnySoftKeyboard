014e18ce742f5177080b3af92dd3a3acdca3c868
Anna Nikulásdóttir <anna@grammatek.com>
2022-09-22 09:30:15 +0000

    lucene autocompleter

--- addons/languages/icelandic/pack/src/main/java/com/anysoftkeyboard/languagepack/icelandic/Autocompleter.java
+++ addons/languages/icelandic/pack/src/main/java/com/anysoftkeyboard/languagepack/icelandic/Autocompleter.java
@@ -1,48 +1,45 @@
 package com.anysoftkeyboard.languagepack.icelandic;
 
 import android.content.Context;
-
-import com.anysoftkeyboard.languagepack.icelandic.pack.R;
+import android.util.Log;
 
 import org.apache.lucene.search.suggest.fst.FSTCompletion;
 import org.apache.lucene.search.suggest.fst.FSTCompletionBuilder;
 import org.apache.lucene.util.BytesRef;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Autocompleter {
+    final static String TAG = "ASK_ICE_Autocompleter";
     final static List<Integer> BUCKETS = Arrays.asList(10, 50, 100, 500, 1000, 5000, 10000, 20000, 40000, 10000000);
     FSTCompletion completion;
 
-    Context mContext;
-
-    public Autocompleter(Context context) throws IOException {
-        mContext = context;
-        List<String> freqdictContent = readFile();
-        FSTCompletionBuilder FSTbuilder = new FSTCompletionBuilder();
-        for (String line : freqdictContent) {
-            String[] arr = line.split("\t");
-            BytesRef term = new BytesRef(arr[0]);
-            int freq = Integer.parseInt(arr[1]);
-            int bucket = 0;
-            for (int b : BUCKETS) {
-                int ind = BUCKETS.indexOf(b);
-                if (freq > b && freq < BUCKETS.get(ind + 1)) {
-                    bucket = ind;
-                    break;
+    @SuppressWarnings("StringSplitter")
+    public Autocompleter()  {
+        try {
+            List<String> freqdictContent = readFile();
+            FSTCompletionBuilder FSTbuilder = new FSTCompletionBuilder();
+            for (String line : freqdictContent) {
+                String[] arr = line.split("\t");
+                BytesRef term = new BytesRef(arr[0]);
+                int freq = Integer.parseInt(arr[1]);
+                int bucket = 0;
+                for (int b : BUCKETS) {
+                    int ind = BUCKETS.indexOf(b);
+                    if (freq > b && freq < BUCKETS.get(ind + 1)) {
+                        bucket = ind;
+                        break;
+                    }
                 }
+                FSTbuilder.add(term, bucket);
             }
-            FSTbuilder.add(term, bucket);
+            completion = FSTbuilder.build();
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
         }
-        completion = FSTbuilder.build();
     }
 
     public List<String> autocomplete(String input, int maxNum) {
@@ -54,12 +51,9 @@ public class Autocompleter {
         return results;
     }
 
-
-
     private List<String> readFile() {
-        final List<String> fileContent = FileUtils.readLinesFromResourceFile(this.mContext,
-                R.raw.uni_bigrams);
-
+        FileUtils fileUtils = new FileUtils();
+        final List<String> fileContent = fileUtils.readLinesFromResourceFile("res/raw/uni_bigrams.tsv");
         return fileContent;
     }
 
--- addons/languages/icelandic/pack/src/main/java/com/anysoftkeyboard/languagepack/icelandic/FileUtils.java
+++ addons/languages/icelandic/pack/src/main/java/com/anysoftkeyboard/languagepack/icelandic/FileUtils.java
@@ -2,36 +2,43 @@ package com.anysoftkeyboard.languagepack.icelandic;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.util.Log;
+
+import androidx.annotation.RawRes;
+
+import com.anysoftkeyboard.languagepack.icelandic.pack.R;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtils {
+    private static final String TAG = "ASK_ICE_FileUtils";
 
     /**
      * Reads a resource file with the given resID, e.g. R.raw.uni_bigrams.tsv
      *
      * Returns the file content as a list of strings.
      *
-     * @param context
-     * @param resID the resource id
      * @return a list of strings representing file content
      */
-    public static List<String> readLinesFromResourceFile(Context context, int resID) {
-        Resources res = context.getResources();
+    @SuppressWarnings("CatchAndPrintStackTrace")
+    public List<String> readLinesFromResourceFile(String filename) {
         List<String> fileContent = new ArrayList<>();
         String line = "";
         try {
-            InputStream is = res.openRawResource(resID);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(filename);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
             if (is != null) {
                 while ((line = reader.readLine()) != null) {
                     fileContent.add(line);
                 }
             }
+            reader.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
